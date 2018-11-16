868bb7667c5bf4bb8270ac23ad597fda289cc98e
tzattack <tzattack@gmail.com>
2018-11-16 13:30:31 +0800

    search insert position solved

---
+++ search-insert-position.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i, num in enumerate(nums):
+            if target <= num:
+                return i
+        return len(nums)
