ac8bf1ff3868eb4da058041f69a6812318526b92
tzattack <tzattack@gmail.com>
2020-10-29 19:59:15 +0800

    8.1

--- 8.字符串转换整数-atoi.py
+++ 8.字符串转换整数-atoi.py
@@ -10,20 +10,36 @@ class Solution:
         digits = ["0","1","2","3","4","5","6","7","8","9"]
         sign = 1
         value = ""
+        signFlag = 1
         for i in range(len(s)):
-            if s[i] == " ": continue
-            elif s[i] == "-": sign = -1
+            if s[i] == " ": 
+                if value=="":
+                    continue
+                else:
+                    return 0
+            elif s[i] == "-": 
+                if signFlag:
+                    sign = -1
+                    signFlag = 0
+                else:
+                    return 0
+            elif s[i] == "+": 
+                if signFlag: 
+                    sign = 1
+                    signFlag = 0
+                else:
+                    return 0
             elif s[i] in digits:
                 value += s[i]
             else:
                 break
-
+        print(value)
         if value == "": return 0
         ans = sign*(int(value))
         if ans < -2**31:
             return -2**31
-        elif ans > 2**32:
-            return 2**32
+        elif ans > 2**31:
+            return 2**31-1
         else:
             return ans
 
